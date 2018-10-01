58b0bbd16b548d6fcce8ea53d8c6fb34696174d5
adyyousf <adnan.77-cse-18@mietjammu.in>
2018-10-01 20:56:29 +0530

    adding abc.c

---
+++ abc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main(int argc, char const *argv[])
+{
+	int a = 1, b = 1, c = 0 , i;
+	do
+	{
+		printf("%d\n %d\n", a, b );
+		/*a = b;
+		b = c;
+		c = a+b;*/
+	} while (a = 0, a<20);
+
+	return 0;
+}
